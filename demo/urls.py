5f1e31219565b22ddedb7f05320caf20454131b5
Dusai <dusaiphoto@foxmail.com>
2020-04-21 16:01:31 +0800

    Reverse()

--- demo/urls.py
+++ demo/urls.py
@@ -1,23 +1,27 @@
 from django.urls import path
 from .views import (
-    ReverseToSomePageView,
+    ReverseView,
     HomePageWithContextView,
-    HomePageWithContextAndParamsView,
 )
 
 app_name = 'demo'
 
 urlpatterns = [
-
+    # MARK: - Reverse()
     path('reverse/',
-         ReverseToSomePageView.as_view(),
+         ReverseView.as_view(),
+         name='reverse'),
+
+    path('reverse/<int:id>/',
+         ReverseView.as_view(),
          name='reverse'),
 
     path('home-with-context/',
          HomePageWithContextView.as_view(),
          name='home_with_context'),
 
-    path('home-with-context-and-params/<int:id>/',
-         HomePageWithContextAndParamsView.as_view(),
-         name='home_with_context_and_params'),
+    path('home-with-context/<int:id>/',
+         HomePageWithContextView.as_view(),
+         name='home_with_context'),
+
 ]
--- demo/views.py
+++ demo/views.py
@@ -9,24 +9,28 @@ class HomePageView(View):
         return render(request, 'home.html', context={})
 
 
-class ReverseToSomePageView(View):
-    def get(self, request):
-        url = reverse('demo:home_with_context')
+class ReverseView(View):
+    def get(self, request, *args, **kwargs):
+        id = kwargs.get('id')
+
+        if id == None:
+            # 不带参数
+            url = reverse('demo:home_with_context')
+        else:
+            # 带有参数
+            url = reverse('demo:home_with_context', args=(id,))
         return redirect(url)
 
 
 class HomePageWithContextView(View):
-    def get(self, request):
-        context = {'content': '我从 Reverse() 回来'}
-        return render(request, 'home.html', context=context)
-
-
-class HomePageWithContextAndParamsView(View):
     def get(self, request, *args, **kwargs):
         id = kwargs.get('id')
+
         if id == 0:
             context = {'content': '我从 Url 模板语法解析回来，并带有参数 id - {} 哦'.format(id)}
-        else:
+        elif id == 1:
             context = {'content': '我从 Reverse() 回来，并带有参数 id - {} 哦'.format(id)}
+        else:
+            context = {'content': '我从 Reverse() 回来'}
 
         return render(request, 'home.html', context=context)
